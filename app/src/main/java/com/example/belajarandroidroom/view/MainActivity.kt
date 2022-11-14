07bd405081e02b2c1f3817e823bff9c6caee99de
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-14 21:14:53 +0700

    Adding soft back button in activity action bar.

--- app/src/main/java/com/example/belajarandroidroom/view/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroidroom/view/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.belajarandroidroom.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.belajarandroidroom.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.fragmentHost)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
