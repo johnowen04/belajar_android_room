94c73250dd11b5879c2039879fe3600e943d3209
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-14 20:20:45 +0700

    Using DetailTodoViewModel in CreateTodoFragment.kt

--- app/src/main/java/com/example/belajarandroidroom/view/CreateTodoFragment.kt
+++ app/src/main/java/com/example/belajarandroidroom/view/CreateTodoFragment.kt
@@ -5,13 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import androidx.navigation.Navigation
 import com.example.belajarandroidroom.R
+import com.example.belajarandroidroom.model.Todo
+import com.example.belajarandroidroom.viewmodel.DetailTodoViewModel
+import kotlinx.android.synthetic.main.fragment_create_todo.*
+
 class CreateTodoFragment : Fragment() {
+    private lateinit var viewModel: DetailTodoViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_todo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailTodoViewModel::class.java)
+
+        btnAddNotes.setOnClickListener {
+            val todo = Todo(
+                editTitle.text.toString(),
+                editNotes.text.toString()
+            )
+
+            val list = listOf(todo)
+
+            viewModel.addTodo(list)
+            Toast.makeText(view.context, "Data added", Toast.LENGTH_SHORT).show()
+            Navigation.findNavController(it).popBackStack()
+        }
     }
 }
