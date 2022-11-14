441d96f52ea51463832328e7d58d94653b7a784f
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-14 20:35:57 +0700

    Creating TodoListAdapter and using it in TodoListFragment. Also using TodoListViewModel in TodoListFragment.

---
+++ app/src/main/java/com/example/belajarandroidroom/view/TodoListAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.belajarandroidroom.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.belajarandroidroom.R
+import com.example.belajarandroidroom.model.Todo
+import kotlinx.android.synthetic.main.todo_item_layout.view.*
+
+class TodoListAdapter(val todoList: ArrayList<Todo>): RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(var view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.todo_item_layout, parent, false)
+
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val todo = todoList[position]
+
+        with(holder.view) {
+            this.checkBox.text = todo.title
+        }
+    }
+
+    fun updateTodoList(newTodoList: List<Todo>) {
+        todoList.clear()
+        todoList.addAll(newTodoList)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = todoList.size
+}
--- app/src/main/java/com/example/belajarandroidroom/view/TodoListFragment.kt
+++ app/src/main/java/com/example/belajarandroidroom/view/TodoListFragment.kt
@@ -5,13 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.belajarandroidroom.R
+import com.example.belajarandroidroom.viewmodel.TodoListViewModel
+import kotlinx.android.synthetic.main.fragment_todo_list.*
+
 class TodoListFragment : Fragment() {
+    private lateinit var viewModel: TodoListViewModel
+
+    private lateinit var adapter: TodoListAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_todo_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(TodoListViewModel::class.java)
+        viewModel.refresh()
+
+        adapter = TodoListAdapter(arrayListOf())
+        recTodo.layoutManager = LinearLayoutManager(context)
+        recTodo.adapter = adapter
+
+        fabTodo.setOnClickListener {
+            val action = TodoListFragmentDirections.actionCreateTodoFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.todoListLiveData.observe(viewLifecycleOwner) {
+            adapter.updateTodoList(it)
+            txtEmpty.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
+        }
     }
 }
