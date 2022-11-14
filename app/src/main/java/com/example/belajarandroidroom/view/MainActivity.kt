82b06c5697a96ec08f14a3d8e710d642ba345851
John Owen Danuarta <john.owen.d19@gmail.com>
2022-11-14 18:49:57 +0700

    Create package for MVVM architecture. Add two new vector assets and create two fragments, TodoListFragment and CreateTodoFragment.

--- app/src/main/java/com/example/belajarandroidroom/view/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroidroom/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.belajarandroidroom
+package com.example.belajarandroidroom.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.belajarandroidroom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
